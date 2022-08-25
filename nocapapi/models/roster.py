6bc3764ab76b0d976c88f6949e800aa7a186fc88
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-08-25 11:35:48 -0500

    added roster edit for naming, models + name field null

--- nocapapi/models/roster.py
+++ nocapapi/models/roster.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class Roster(models.Model):
     user = models.ForeignKey("RosterUser", on_delete=models.CASCADE)
+    name = models.CharField(max_length=800, null=True)
