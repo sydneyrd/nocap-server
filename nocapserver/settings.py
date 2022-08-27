cff7e7fcf2d0451651a631890cdbfe6d46e73f71
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-08-27 08:04:07 -0500

    took out the first and last names for users on registration and profile

--- nocapserver/settings.py
+++ nocapserver/settings.py
@@ -143,3 +143,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
