422e55556e5081ec956cd3e19d7ac71cf866ce5e
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-09-28 12:14:26 -0500

    removed mandatory roster assignment on charlink

--- nocapapi/models/charlink.py
+++ nocapapi/models/charlink.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class CharLink(models.Model):
     character = models.ForeignKey("Character", on_delete=models.CASCADE, related_name="character")
-    roster = models.ForeignKey("CalculatedRoster", on_delete=models.CASCADE, related_name="link_roster")
+    roster = models.ForeignKey("CalculatedRoster", on_delete=models.CASCADE, null=True, related_name="link_roster")
     link = models.URLField("link", max_length=500)
