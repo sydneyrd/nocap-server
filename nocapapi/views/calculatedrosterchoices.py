fa3552320136df5caf3fa74114f9558762bc4f9a
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-02-10 13:23:07 -0600

    spelling errors

--- nocapapi/views/calculatedrosterchoices.py
+++ nocapapi/views/calculatedrosterchoices.py
@@ -3,83 +3,92 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from nocapapi.models import CalculatedRosterChoices, Character, CalculatedRoster
-from django.db.models import Sum, Aggregate
+
 
 
 
 class CalculatedRosterChoicesView(ViewSet):
-    """Level up game types view"""
-
+    """Calculated Roster Choices/ the characters on rosters view"""
     def retrieve(self, request, pk):
-        """Handle GET requests for single game type
+        """Handle GET requests for single calculated roster choice
         Returns:
-            Response -- JSON serialized game type"""
+            Response -- JSON serialized calculated roster choice"""
         try:
-            calcrostchoices = CalculatedRosterChoices.objects.get(pk=pk)
-            serializer = CalcRostChoicesSerializer(calcrostchoices)
+            calculated_roster_choices = CalculatedRosterChoices.objects.get(pk=pk)
+            serializer = CalcRostChoicesSerializer(calculated_roster_choices)
             return Response(serializer.data)
         except CalculatedRosterChoices.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-        
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     def list(self, request):
-        """Handle GET requests to get all game types
+        """Handle GET requests to get all calculated roster choices
         Returns:
-        Response -- JSON serialized list of game types
+        Response -- JSON serialized list of calculated roster choices
     """
-        calcrostchoices = CalculatedRosterChoices.objects.all()
-        calcroster = request.query_params.get('calculatedroster', None)
-        if calcroster is not None:
-            calcrostchoices = calcrostchoices.filter(calculated_roster=calcroster)
-    
-        serializer = CalcRostChoicesSerializer(calcrostchoices, many=True)
-        return Response(serializer.data)
+        try:
+            calculated_roster_choices = CalculatedRosterChoices.objects.all()
+            calculated_roster = request.query_params.get('calculatedroster', None)
+            if calculated_roster is not None:
+                calculated_roster_choices = calculated_roster_choices.filter(calculated_roster=calculated_roster)
         
+            serializer = CalcRostChoicesSerializer(calculated_roster_choices, many=True)
+            return Response(serializer.data)
+        except CalculatedRosterChoices.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     def create(self, request):
-        """Handle POST operations"""
-        calculatedroster = CalculatedRoster.objects.get(pk=request.data['calculated_roster'])
+        """Handle POST operations for calculated roster choices"""
+        calculated_roster = CalculatedRoster.objects.get(pk=request.data['calculated_roster'])
         character = Character.objects.get(pk=request.data['character'])
-
-        if request.data['group'] != 0:
-            newrosterchoice = CalculatedRosterChoices.objects.create(
-                calculated_roster=calculatedroster,
-                character=character,
-                damage=request.data['damage'],
-                healing=request.data['healing'],
-                kills=request.data['kills'],
-                deaths=request.data['deaths'],
-                assists=request.data['assists'],
-                group=request.data['group']
-        )
-        else:
-            newrosterchoice=CalculatedRosterChoices.objects.create(
-                    calculated_roster=calculatedroster,
+        try:
+            if request.data['group'] != 0:
+                new_roster_choice = CalculatedRosterChoices.objects.create(
+                    calculated_roster=calculated_roster,
                     character=character,
                     damage=request.data['damage'],
                     healing=request.data['healing'],
                     kills=request.data['kills'],
                     deaths=request.data['deaths'],
-                    assists=request.data['assists']
+                    assists=request.data['assists'],
+                    group=request.data['group']
             )
+            else:
+                new_roster_choice=CalculatedRosterChoices.objects.create(
+                        calculated_roster=calculated_roster,
+                        character=character,
+                        damage=request.data['damage'],
+                        healing=request.data['healing'],
+                        kills=request.data['kills'],
+                        deaths=request.data['deaths'],
+                        assists=request.data['assists']
+                )
 
-        serializer = CalcRostChoicesSerializer(newrosterchoice)
-        return Response(serializer.data)
-
+            serializer = CalcRostChoicesSerializer(new_roster_choice)
+            return Response(serializer.data)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     def update(self, request, pk):
         """Handle PUT requests for a calcRosterChoice
         Returns:
             Response -- Empty body with 204 status code
         """
-        calcrostchoices = CalculatedRosterChoices.objects.get(pk=pk)
-        calcrostchoices.damage = request.data['damage']
-        calcrostchoices.healing = request.data['healing']
-        calcrostchoices.kills = request.data['kills']
-        calcrostchoices.deaths = request.data['deaths']
-        calcrostchoices.assists = request.data['assists']
-        if 'group' in request.data:
-            calcrostchoices.group = request.data['group']
-        calcrostchoices.save()
-
-        return Response({"character updated"}, status=status.HTTP_204_NO_CONTENT)
+        try:
+            calculated_roster_choice = CalculatedRosterChoices.objects.get(pk=pk)
+            calculated_roster_choice.damage = request.data['damage']
+            calculated_roster_choice.healing = request.data['healing']
+            calculated_roster_choice.kills = request.data['kills']
+            calculated_roster_choice.deaths = request.data['deaths']
+            calculated_roster_choice.assists = request.data['assists']
+            if 'group' in request.data:
+                calculated_roster_choice.group = request.data['group']
+            calculated_roster_choice.save()
+            return Response({"character updated"}, status=status.HTTP_204_NO_CONTENT)
+        except CalculatedRosterChoices.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     def destroy(self, request, pk):
         """Handle DELETE requests for a single calcRosterChoice
         Returns:
@@ -96,15 +105,12 @@ class CalculatedRosterChoicesView(ViewSet):
 
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-
 class CalculatedRosterSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
     class Meta:
         model = CalculatedRoster
         fields = ('id', 'rosterName', 'roster' )
-
 class CharacterSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
@@ -120,7 +126,6 @@ class CharacterSerializer(serializers.ModelSerializer):
         "secondary_weapon",
         "server",
         "user")
-
 class CalcRostChoicesSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
