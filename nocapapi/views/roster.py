cbd8b8029c43508f72673f5c882c9bd4b01e58cf
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-02-12 10:07:09 -0600

    roster tests

--- nocapapi/views/roster.py
+++ nocapapi/views/roster.py
@@ -22,20 +22,67 @@ class RosterView(ViewSet):
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
         
 
+    # def list(self, request):
+    #     """Handle GET requests to get all rosters
+    #     Returns:
+    #         Response -- JSON serialized list of rosters
+    #     """
+    #     try:
+    #         roster = Roster.objects.all()
+    #         roster_user = request.query_params.get('user', None)
+    #     except Roster.DoesNotExist as ex:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    #     except Exception as ex:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    #     if roster_user is not None:
+    #         roster = roster.filter(user=roster_user)
+    #         serializer = RosterSerializer(roster, many=True)
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     else:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    # def list(self, request):
+    #     """Handle GET requests to get all rosters
+    #     Returns:
+    #         Response -- JSON serialized list of rosters
+    #     """
+    #     try:
+    #         roster = Roster.objects.all()
+    #         str_user = request.query_params.get('user', None)
+    #         roster_user = RosterUser.objects.get(pk=str_user)
+    #         if roster_user is not None:
+    #             roster = roster.filter(user=roster_user)
+    #             serializer = RosterSerializer(roster, many=True)
+    #             return Response(serializer.data, status=status.HTTP_200_OK)
+    #     except Roster.DoesNotExist as ex:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    #     except RosterUser.DoesNotExist as ex:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    #     except Exception as ex:
+    #         return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
     def list(self, request):
         """Handle GET requests to get all rosters
-        Returns:
-            Response -- JSON serialized list of rosters
-        """
+    Returns:
+        Response -- JSON serialized list of rosters
+    """
         try:
             roster = Roster.objects.all()
-            roster_user = request.query_params.get('user', None)
+            str_user = request.query_params.get('user', None)
+            roster_user = RosterUser.objects.get(pk=str_user)
             if roster_user is not None:
                 roster = roster.filter(user=roster_user)
-            serializer = RosterSerializer(roster, many=True)
-        except Exception as ex:
+                serializer = RosterSerializer(roster, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        except Roster.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-        return Response(serializer.data)
+        except RosterUser.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            
     def create(self, request):
         """Handle POST operations for rosters"""
         try:
--- tests/roster_tests.py
+++ tests/roster_tests.py
@@ -3,22 +3,9 @@ from rest_framework.test import APITestCase
 from nocapapi.models import Roster
 from nocapapi.views.roster import RosterSerializer
 
+
 class RosterTests(APITestCase):
-    def test_get_roster(self):
-        """Get single roster test"""
-        roster = Roster.objects.first()
-        response = self.client.get(f"/rosters/{roster.pk}")
-        expected = RosterSerializer(roster)
-        self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(expected.data, response.data)
-    def test_get_rosters(self):
-        """Get rosters test"""
-        url = "/rosters"
-        response = self.client.get(url)
-        rosters = Roster.objects.all()
-        expected = RosterSerializer(rosters, many=True)
-        self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual(expected.data, response.data)
+    
     def test_create_roster(self):
         """Create roster test"""
         url = "/rosters"
@@ -30,3 +17,68 @@ class RosterTests(APITestCase):
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual(data["name"], response.data["name"])
         self.assertEqual(data["user"], response.data["user"])
+    def test_get_roster(self):
+        """Get single roster test"""
+        roster = Roster.objects.first()
+        response = self.client.get(f"/rosters/{roster.pk}")
+        expected = RosterSerializer(roster)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(expected.data, response.data)
+    def test_get_rosters(self):
+        """Get rosters test"""
+        url = f"/rosters?user={self.rosteruser.pk}"
+        response = self.client.get(url)
+        rosters = Roster.objects.filter(user=self.rosteruser)
+        expected = RosterSerializer(rosters, many=True)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(expected.data, response.data)
+    def test_update_roster(self):
+        """Update roster test"""
+        roster = Roster.objects.first()
+        url = f"/rosters/{roster.pk}"
+        data = {
+            "name": "Test Roster",
+            "user": self.rosteruser.pk
+        }
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(None, response.data)
+    def test_create_roster_no_name(self):
+        """Create roster no name test"""
+        url = "/rosters"
+        data = {
+            "user": self.rosteruser.pk
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(data["user"], response.data["user"])
+    def test_delete_roster(self):
+        """Delete roster test"""
+        roster = Roster.objects.first()
+        url = f"/rosters/{roster.pk}"
+        response = self.client.delete(url)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(None, response.data)
+    def test_delete_roster_not_found(self):
+        """Delete roster not found test"""
+        url = f"/rosters/0"
+        response = self.client.delete(url)
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+    def test_get_roster_not_found(self):
+        """Get roster not found test"""
+        url = f"/rosters/0"
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+    def test_get_rosters_by_user(self):
+        """Get rosters by user test"""
+        url = f"/rosters?user={self.rosteruser.pk}"
+        response = self.client.get(url)
+        rosters = Roster.objects.filter(user=self.rosteruser.pk)
+        expected = RosterSerializer(rosters, many=True)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(expected.data, response.data)
+    def test_get_rosters_by_user_not_found(self):
+        """Get rosters by user not found test"""
+        url = f"/rosters?user=0"
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
