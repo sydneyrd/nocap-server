67e9a77d46b50127f0c44ad936737db2891f4ca4
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-02-13 05:14:17 -0600

    link get all tests

--- tests/char_link_tests.py
+++ tests/char_link_tests.py
@@ -21,3 +21,90 @@ class CharLinkTests(APITestCase):
         self.assertEqual(data["calculated_roster"],
                         response.data["calculated_roster"])
         self.assertEqual(data["link"], response.data["link"])
+    def test_get_char_link_by_character(self):
+        """Get character link by character test"""
+        url = "/links"
+        data = {
+            "character": Character.objects.first().pk,
+            "calculated_roster": CalculatedRoster.objects.first().pk,
+            "link": "https://www.google.com"
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(data["character"], response.data["character"])
+        self.assertEqual(data["calculated_roster"],
+                        response.data["calculated_roster"])
+        self.assertEqual(data["link"], response.data["link"])
+        url = f"/links?character={Character.objects.first().pk}"
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(data["character"], response.data[0]["character"])
+        self.assertEqual(data["calculated_roster"],
+                        response.data[0]["calculated_roster"])
+        self.assertEqual(data["link"], response.data[0]["link"])
+    def test_delete_char_link(self):
+        """Delete character link test"""
+        url = "/links"
+        data = {
+            "character": Character.objects.first().pk,
+            "calculated_roster": CalculatedRoster.objects.first().pk,
+            "link": "https://www.google.com"
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(data["character"], response.data["character"])
+        self.assertEqual(data["calculated_roster"],
+                        response.data["calculated_roster"])
+        self.assertEqual(data["link"], response.data["link"])
+        url = f"/links/{CharLink.objects.first().pk}"
+        response = self.client.delete(url)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(None, response.data)
+    def test_get_char_link_by_character_fail(self):
+        """Get character link by character fail test"""
+        url = "/links"
+        data = {
+            "character": Character.objects.first().pk,
+            "calculated_roster": CalculatedRoster.objects.first().pk,
+            "link": "https://www.google.com"
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(data["character"], response.data["character"])
+        self.assertEqual(data["calculated_roster"],
+                        response.data["calculated_roster"])
+        self.assertEqual(data["link"], response.data["link"])
+        url = f"/links?character={Character.objects.last().pk}"
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+        self.assertEqual("character not found", response.data["message"])
+    def test_get_all_char_links(self):
+        """Get all character links test"""
+        url = "/links"
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
+
+
+    # def test_get_char_link_by_calculated_roster(self):
+    #     """Get character link by calculated roster test"""
+    #     url = "/links"
+    #     data = {
+    #         "character": Character.objects.first().pk,
+    #         "calculated_roster": CalculatedRoster.objects.first().pk,
+    #         "link": "https://www.google.com"
+    #     }
+    #     response = self.client.post(url, data, format="json")
+    #     self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+    #     self.assertEqual(data["character"], response.data["character"])
+    #     self.assertEqual(data["calculated_roster"],
+    #                     response.data["calculated_roster"])
+    #     self.assertEqual(data["link"], response.data["link"])
+    #     url = f"/links?calculated_roster={CalculatedRoster.objects.first().pk}"
+    #     response = self.client.get(url)
+    #     self.assertEqual(status.HTTP_200_OK, response.status_code)
+    #     self.assertEqual(data["character"], response.data[0]["character"])
+    #     self.assertEqual(data["calculated_roster"],
+    #                     response.data[0]["calculated_roster"])
+    #     self.assertEqual(data["link"], response.data[0]["link"])
+
+    # need to add to view
