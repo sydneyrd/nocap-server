4700e9a2262fe78bda6017d881487cd4db9eb873
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-04-05 17:18:41 -0500

    mmmmhmmm

--- nocapapi/serializers/character_filter.py
+++ nocapapi/serializers/character_filter.py
@@ -11,12 +11,12 @@ class CharacterSerializer(serializers.ModelSerializer):
         fields = ('id', 'role', 'faction', 'primary_weapon', 'secondary_weapon', 'server',  'character_name', 'user', 'notes', 'image' )
 
 
-class CharacterFilterSerializer(serializers.Serializer):
-    role_id = serializers.IntegerField(required=False)
-    faction_id = serializers.IntegerField(required=False)
-    primary_weapon_id = serializers.IntegerField(required=False)
-    secondary_weapon_id = serializers.IntegerField(required=False)
-    server_id = serializers.IntegerField(required=False)
-    character_name = serializers.CharField(required=False)
-    user_id = serializers.IntegerField(required=False)
-    search_text = serializers.CharField(required=False)
+# class CharacterFilterSerializer(serializers.Serializer):
+#     role_id = serializers.IntegerField(required=False)
+#     faction_id = serializers.IntegerField(required=False)
+#     primary_weapon_id = serializers.IntegerField(required=False)
+#     secondary_weapon_id = serializers.IntegerField(required=False)
+#     server_id = serializers.IntegerField(required=False)
+#     character_name = serializers.CharField(required=False)
+#     user_id = serializers.IntegerField(required=False)
+#     search_text = serializers.CharField(required=False)
