e6b180042b9a42e452de46beb32079d35147ba90
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-04-08 12:33:53 -0500

    fixed wrong links being attached to boards

--- nocapapi/serializers/calculated_roster_choice.py
+++ nocapapi/serializers/calculated_roster_choice.py
@@ -7,14 +7,13 @@ from nocapapi.serializers.charlink import CharLinkSerializer
 class CalcRostChoicesSerializer(serializers.ModelSerializer):
     """JSON serializer for calcrosterchoices 
     """
-    # calculated_roster = CalculatedRosterSerializer(many=False)
     character = CharacterSerializer(many=False)
-    char_links = CharLinkSerializer(many=True, read_only=True, source='character.character_links')
-
-    # i need to any charlinks that are associated with this calculatedroster via the character and then the calculated roster also
-
-    
+    char_links = serializers.SerializerMethodField()
 
     class Meta:
         model = CalculatedRosterChoices
-        fields = ('id', 'character',  'damage', 'healing', 'kills', 'deaths', 'assists', 'group', 'char_links' )
+        fields = ('id', 'character', 'damage', 'healing', 'kills', 'deaths', 'assists', 'group', 'char_links')
+
+    def get_char_links(self, obj):
+        char_links = obj.character.character_links.filter(calculated_roster=obj.calculated_roster)
+        return CharLinkSerializer(char_links, many=True, read_only=True).data
