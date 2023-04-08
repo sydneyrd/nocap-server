ae9fd1bc003a4b84bdecd3e26af0885f2b01caf7
Sydney Dickson <sydneyrachelledickson@gmail.com>
2023-04-08 11:15:14 -0500

    i think it's working

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
-python3 manage.py migrate
-python3 manage.py loaddata weapons.json servers.json roles.json factions.json 
+# python3 manage.py migrate
+# python3 manage.py loaddata weapons.json servers.json roles.json factions.json 
